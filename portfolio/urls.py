127cbc720183bfdb682cd280101cf48ff1f38b62
sachix1001 <sachix1001@gmail.com>
2020-02-26 21:36:36 +0900

    add detail page

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',jobs.views.home, name='home'),
-
+    path('jobs/<int:job_id>',jobs.views.detail, name='detail')
 ]
 if settings.DEBUG is True:
     urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
